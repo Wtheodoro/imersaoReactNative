9c167d6a084ef61593bc3b2c2c839d1feb6e9770
WTheodoro <theodoro.walison@gmail.com>
2021-01-06 18:24:59 -0300

    primeira parte da segunda aula

---
+++ README.md
@@ -0,0 +1,17 @@
+SEQUENCIA PARA CRIAR O PROJETO
+Criar o projeto com React Native
+### expo init NOME_DO_PROJETO
+
+Rodar o projeto
+### expo star
+ou
+### npm start
+
+Instalar o React Navigation
+### npm install @react-navigation/native
+
+Dependências do React Navigation
+### expo install react-native-gesture-handler react-native-reanimated react-native-screens react-native-safe-area-context @react-native-community/masked-view
+
+Instalar o React Navigation Stack
+### npm install @react-navigation/stack
---
+++ src/routes.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Home from './pages/Home'
+import Metas from './pages/Metas'
+
+const Stack = createStackNavigator()
+
+export default function Routes() {
+
+    const screenOptionStyle = {
+        headerStyle: {
+            backgroundColor: '#171941'
+        },
+        headerTintColor: '#bf38ac'
+    }
+    return (
+        <NavigationContainer>
+            <Stack.Navigator screenOptions={screenOptionStyle}>
+                <Stack.Screen name="Home" component={Home} />
+                <Stack.Screen name="Metas" component={Metas} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
